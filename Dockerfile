d9e619632b951e9197f9958cb0d53007327a4555
mmmirana <18166748035@163.com>
2019-11-28 14:26:15 +0800

    修复docker env读取arg变量失败的bug3: jenkins自动化构建镜像需要传入参数APP_NAME和APP_JAR_FILE，在docker_build_run.sh中指定

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,12 @@ MAINTAINER "mmmirana<18166748035@163.com>"
 LABEL app.name="springboot_dashboard"
 LABEL app.version="1.0"
 
-# 获取app名称
+# app名称
 ARG APP_NAME
-# 获取<app>.jar名称
+# <app>.jar名称
 ARG APP_JAR_FILE
 
 # 指定加载app.jar的位置
-ENV APP_NAME=${APP_NAME}
-ENV APP_JAR_FILE=${APP_JAR_FILE}
 ENV ENTRY_APP_JAR_PATH=/mmmirana/application/$APP_NAME/$APP_JAR_FILE
 
 RUN echo $ENTRY_APP_JAR_PATH
--- docker_build_run.sh
+++ docker_build_run.sh
@@ -5,11 +5,13 @@ export APP_VERSION=1.1
 # 部署docker时候要暴露给外界的端口号
 export APP_PORT=8003
 # 构建docker的镜像名称+tag
-export DOCKER_IMAGE_NAME=registry.cn-hangzhou.aliyuncs.com/docker_for_k8s/${APP_NAME}:${APP_VERSION}
+#export DOCKER_IMAGE_NAME=registry.cn-hangzhou.aliyuncs.com/docker_for_k8s/$APP_NAME:$APP_VERSION
+export DOCKER_IMAGE_NAME=jenkins.springboot/$APP_NAME:$APP_VERSION
+
 # 运行docker容器的应用名称
-export DOCKER_CONTAINER_NAME="${APP_NAME}"_"${APP_PORT}"
-echo "Jenkins Docker Image      :" $DOCKER_IMAGE_NAME
-echo "Jenkins Docker Container  :" $DOCKER_CONTAINER_NAME
+export DOCKER_CONTAINER_NAME="${APP_NAME}_${APP_PORT}"
+echo "Jenkins Docker Image      :$DOCKER_IMAGE_NAME"
+echo "Jenkins Docker Container  :$DOCKER_CONTAINER_NAME"
 
 # 删除应用
 docker rm -f $DOCKER_CONTAINER_NAME
@@ -17,7 +19,16 @@ docker rm -f $DOCKER_CONTAINER_NAME
 docker rmi -f $DOCKER_IMAGE_NAME
 
 # 构建镜像
-docker build -t $DOCKER_IMAGE_NAME .
+docker build \
+  --build-arg APP_NAME=dashboard \
+  --build-arg APP_JAR_FILE=$APP_NAME-$APP_VERSION.jar \
+  -t \
+  $DOCKER_IMAGE_NAME .
 
 # 运行容器
-docker run -it -d -p $APP_PORT:8080 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+docker run \
+ -it \
+ -d \
+ -p $APP_PORT:8080 \
+ --name $DOCKER_CONTAINER_NAME \
+ $DOCKER_IMAGE_NAME
